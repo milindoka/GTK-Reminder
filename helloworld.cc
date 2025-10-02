cd99b83b97002e4e65c71835e140f27d744b6d0e
milindoka <oak445@gmail.com>
2025-10-02 21:11:06 +0530

    Invisible or transparant window with centre message

--- helloworld.cc
+++ helloworld.cc
@@ -3,12 +3,51 @@
 #include <ctime>
 #include <iomanip>
 #include <sstream>
+#include <gtkmm/application.h> 
+#include <gtkmm/cssprovider.h> 
+#include <gtkmm/stylecontext.h> 
+#include <gdkmm/screen.h> // <-- NEW: Include for Gdk::Screen
 
 HelloWorld::HelloWorld()
-: m_button() // default-construct; label set to current date/time below
+// Initialize the box as vertical
+: m_button(), m_box(Gtk::Orientation::ORIENTATION_VERTICAL) 
 {
   // Sets the border width of the window.
   set_border_width(10);
+  
+  // *** STEP 1: MAXIMIZE THE WINDOW ***
+  maximize(); 
+
+  // *** STEP 2: CONFIGURE TRANSPARENCY AND VISIBILITY (FIXED) ***
+  
+  // 1. Set the window's type to allow transparency
+  set_app_paintable(true);
+  
+  // 2. Load the CSS file
+  auto css_provider = Gtk::CssProvider::create();
+  
+  // Load the style.css file created above. Handle errors if the file is missing.
+  try {
+      css_provider->load_from_path("style.css");
+  } catch (const Glib::Error& ex) {
+      // Print error to standard error stream
+      std::cerr << "Error loading CSS file: " << ex.what() << std::endl;
+  }
+
+  // 3. Apply the CSS to the screen (FIXED LINE)
+  // Get the screen the widget is currently on
+  Glib::RefPtr<Gdk::Screen> screen = get_screen(); 
+  
+  // Use the correct gtkmm method for applying CSS to the screen
+  Gtk::StyleContext::add_provider_for_screen(
+      screen, 
+      css_provider, 
+      GTK_STYLE_PROVIDER_PRIORITY_APPLICATION
+  );
+
+  // OPTIONAL: Remove window decorations (title bar/border)
+  set_decorated(false); 
+  // ******************************************************
 
   // set button label to current date and time
   {
@@ -24,11 +63,13 @@ HelloWorld::HelloWorld()
   m_button.signal_clicked().connect(sigc::mem_fun(*this,
               &HelloWorld::on_button_clicked));
 
-  // This adds the button to the window.
-  add(m_button);
+  // STEP 3: CENTER THE BUTTON 
+  m_box.pack_start(m_button, Gtk::PackOptions::PACK_SHRINK); 
+  m_button.set_halign(Gtk::Align::ALIGN_CENTER);
+  m_box.set_valign(Gtk::Align::ALIGN_CENTER); 
+  add(m_box);
 
-  // The final step is to display this newly created widget.
-  m_button.show();
+  show_all();
 }
 
 HelloWorld::~HelloWorld()
@@ -39,4 +80,3 @@ void HelloWorld::on_button_clicked()
 {
   std::cout << "Hello World" << std::endl;
 }
-
--- helloworld.h
+++ helloworld.h
@@ -1,9 +1,9 @@
-
 #ifndef GTKMM_EXAMPLE_HELLOWORLD_H
 #define GTKMM_EXAMPLE_HELLOWORLD_H
 
 #include <gtkmm/button.h>
 #include <gtkmm/window.h>
+#include <gtkmm/box.h> // <-- NEW: Include Gtk::Box for centering
 
 class HelloWorld : public Gtk::Window
 {
@@ -17,7 +17,8 @@ protected:
 
   //Member widgets:
   Gtk::Button m_button;
+  // NEW: A Gtk::Box to hold and center the button
+  Gtk::Box m_box; 
 };
 
 #endif //GTKMM_EXAMPLE_HELLOWORLD_H
-
