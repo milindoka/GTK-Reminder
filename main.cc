a69a8b9a29933d570cb7d95f7884000d2def07dc
milindoka <oak445@gmail.com>
2025-10-03 08:59:55 +0530

    Auto exit GTK Reminder after 4 seconds

--- main.cc
+++ main.cc
@@ -1,6 +1,8 @@
 #include "helloworld.h"
 #include <gtkmm/application.h>
 
+#include <glibmm/main.h>
+
 int main(int argc, char* argv[])
 {
   // create app with argc/argv (gtkmm3)
@@ -8,6 +10,12 @@ int main(int argc, char* argv[])
 
   // instantiate your window and run the application
   HelloWorld window;
+
+  // Auto-exit the application after 4 seconds (4000 ms)
+  Glib::signal_timeout().connect([app]() {
+    app->quit();
+    return false; // don't repeat
+  }, 4000);
   return app->run(window);
 }
 
